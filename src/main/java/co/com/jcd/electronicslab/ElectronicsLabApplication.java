9131294d94d3ef35e69cf93507ce5aba40ddb82a
Jonny Ferney Bernal Hernández <40707167+jrockb@users.noreply.github.com>
2023-05-10 18:58:17 -0500

    commit inicial ElectronicsLab

---
+++ src/main/java/co/com/jcd/electronicslab/ElectronicsLabApplication.java
@@ -0,0 +1,13 @@
+package co.com.jcd.electronicslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsLabApplication.class, args);
+	}
+
+}
