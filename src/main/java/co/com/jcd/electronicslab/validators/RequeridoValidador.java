a35b953ee933383f06053e6246c530f6b393c7be
Jonny Ferney Bernal Hernández <40707167+jrockb@users.noreply.github.com>
2023-05-17 19:34:46 -0500

    Mejora sobre logica de validador requerido usando StringUtils de spring

--- src/main/java/co/com/jcd/electronicslab/validators/RequeridoValidador.java
+++ src/main/java/co/com/jcd/electronicslab/validators/RequeridoValidador.java
@@ -3,11 +3,13 @@ package co.com.jcd.electronicslab.validators;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.util.StringUtils;
+
 public class RequeridoValidador implements ConstraintValidator<Requerido, String> {
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
-		if(value == null || value.isEmpty() || value.isBlank()) {
+		if(value == null || !StringUtils.hasText(value)) {
 			return false;
 		} 
 		return true;
